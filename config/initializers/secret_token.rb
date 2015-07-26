cd6fa7ec039509cf98f1bd50c458e9c844273feb
victorialomuntad <victoria_lomuntad12@yahoo.com>
2015-07-26 11:50:13 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practiceone::Application.config.secret_key_base = 'b628eed87df48f48ee98daeb67b8b1184f9ca84126eeeb69e79a0300f4e190aa34f2279720e9e02c65ea3a366b8dc8f1f050a1315085f4b50021be4a3df17d94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Practiceone::Application.config.session_store :cookie_store, key: '_practiceone_session'
